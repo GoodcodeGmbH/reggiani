ac7df193ad981af6fcdad4f678e9118e19d359df
Lorenzo Cherubin <53137532+lolelu@users.noreply.github.com>
2024-03-18 16:21:25 +0100

    Reggiani - modifiche 11/03

--- src/components/navbar/navbar-container.tsx
+++ src/components/navbar/navbar-container.tsx
@@ -11,7 +11,7 @@ const NavbarContainer = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <header
-      className={`flex flex-col bg-white lg:flex-row justify-between items-center py-4  px-5 top-0 z-[100] fixed w-full -translate-x-1/2 left-1/2 max-w-screen-4xl `}
+      className={`flex flex-col  bg-[#E6D9C5] lg:flex-row justify-between items-center py-4  px-5 top-0 z-[100] fixed w-full -translate-x-1/2 left-1/2 max-w-screen-4xl `}
       style={
         {
           // backdropFilter: `blur(${scrollYProgress.get() > 0 ? 20 : 0}px)`,
